50073467b0708e4e1b6c04fc877622dea8d211a2
Jesus <jtiradocorro@gmail.com>
2026-03-23 20:30:29 -0600

    Update CORS origins in app.py to include localhost:3000

--- renglo_api/app.py
+++ renglo_api/app.py
@@ -84,7 +84,8 @@ def create_app(config=None, config_path=None):
             origins.extend([
                 "http://127.0.0.1:5173",
                 "http://127.0.0.1:5174",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "http://localhost:3000"
             ])
         
         app.logger.info(f'CORS Origins configured: {origins}')
